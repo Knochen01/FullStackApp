5b7e8dc599bcbff7757359d8faaee8fc20b488bf
Marian Primak <knochen2@gmail.com>
2024-06-29 11:19:15 -0700

    featured

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -1,5 +1,36 @@
-import React from "react";
+import styles from "./featured.module.css";
+import Image from "next/image";
 
 export default function Featured() {
-  return <div>Featured</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, Marian here!</b> Discover my stories and creative ideas.
+      </h1>
+      <div className={styles.wrapper}>
+        <div className={styles.imageWrapper}>
+          <Image
+            className={styles.image}
+            src={"/p1.jpeg"}
+            alt="image"
+            // width={300}
+            // height={300}
+            fill
+          />
+        </div>
+        <div className={styles.textWrapper}>
+          <h1 className={styles.textTitle}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+          </h1>
+          <p className={styles.textDescription}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam sint
+            ab molestias architecto ex assumenda numquam sunt dolor labore
+            cupiditate! Maxime blanditiis nobis id ipsum in sed perspiciatis
+            magni eos?
+          </p>
+          <button className={styles.btn}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
 }
