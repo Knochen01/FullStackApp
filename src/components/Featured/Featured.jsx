6db634d1653c89a6da474fdd1eaa6365b84e3f0d
Marian Primak <knochen2@gmail.com>
2024-06-29 17:04:58 -0700

    categorylist

--- src/components/Featured/Featured.jsx
+++ src/components/Featured/Featured.jsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 export default function Featured() {
   return (
     <div className={styles.container}>
+      <h1 style={{ color: "lime" }}>Featured Component</h1>
       <h1 className={styles.title}>
         <b>Hey, Marian here!</b> Discover my stories and creative ideas.
       </h1>
